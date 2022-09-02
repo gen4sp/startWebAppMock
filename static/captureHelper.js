b3cfdad0f7b53e90e116401c9778644f60b59c01
gen4 <gen4mail@gmail.com>
2022-09-02 17:47:29 +0800

    upd

--- static/captureHelper.js
+++ static/captureHelper.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 window._puppeteerShouldSkipFrameFlag = true;
 document.addEventListener('DOMContentLoaded', function(){ 
-    document.body.startCapture = function(){
+    window._puppeteerstartCapture = function(){
         window._puppeteerShouldSkipFrameFlag = false;
     }
 }, false);
